1dc3c43fa3725e9102dc02199966a8deab5842c1
BertheAdrien <berthe.adrien@yahoo.fr>
2025-09-10 10:19:58 +0200

    BDD trous

---
+++ database/migrations/2025_09_10_081724_create_trous_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('trous', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->float('depht'); 
+            $table->float('volume');
+            $table->string('location');
+            $table->float('diameter');
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('trous');
+    }
+};
