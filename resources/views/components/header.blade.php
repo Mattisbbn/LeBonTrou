7a69488a882bef8a464d0ab3dba6f13e5b1ea33a
Mattisbbn <mattisbbn@gmail.com>
2025-09-10 12:24:37 +0200

    upd

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,15 @@
+<header class="bg-black-100 text-white">
+
+    <div class="w-11/12 mx-auto py-2 flex justify-between items-center">
+
+        <div>
+            <h1 class="text-3xl font-bold">leBonTrou</h1>
+        </div>
+
+        <div class="flex gap-6">
+            <a href="#">Catalogue</a>
+            <a href="#">Cours HooleyCoin</a>
+            <a href="#">A propos</a>
+        </div>
+    </div>
+</header>
--- resources/views/pages/home/index.blade.php
+++ resources/views/pages/home/index.blade.php
@@ -1,3 +1,25 @@
 <x-layouts.guest>
-test
+
+
+
+<main>
+    <section class="flex bg-gradient-to-br from-void-950 via-deep-950 to-portal-600 h-[700px] relative overflow-hidden text-white">
+        <div class="w-11/12 m-auto flex ">
+            <div class="max-w-7xl mx-auto px-6 py-20">
+                <div class="space-y-8">
+                    <h1 class="text-5xl font-bold">Le trou n'est plus une<span class="text-transparent bg-clip-text bg-gradient-to-r from-blue-ribbon-700 to-medium-purple-500"><br>absence de matière</span></h1>
+                    <p class="text-xl my-4">C'est une ressource stratégique.</p>
+                    <p class="text-white/75 text-lg mt-6">Découvrez le premier marché mondial de trous certifiés. De la singularité cosmique au simple trou de serrure.</p>
+
+                    <button class="bg-portal px-8 py-4 rounded-lg text-lg font-semibold hover:bg-blue-700 transition-colors">En savoir plus</button>
+                </div>
+            </div>
+            <img src="{{ asset('images/home/blackhole.webp') }}" alt="Home" class="w-full h-96 object-cover rounded-2xl shadow-2xl">
+        </div>
+
+    </section>
+</main>
+
+
+
 </x-layouts.guest>
