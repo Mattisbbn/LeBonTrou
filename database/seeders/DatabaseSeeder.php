bee155e7dbfca533ef700639156732645c79e5f1
BertheAdrien <berthe.adrien@yahoo.fr>
2025-09-10 12:26:21 +0200

    ajout categorie 1

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,18 +6,36 @@ use Illuminate\Database\Seeder;
 use App\Models\Trou;
 use App\Models\Wind;
 use App\Models\Category;
+use App\Models\TrouImage;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        Category::factory(5)->create()->each(function ($category) {
-            Trou::factory(3)->create([
-                'category_id' => $category->id,
-            ]);
-        });
 
         // 50 mesures de vent
         Wind::factory(50)->create();
+
+        Category::factory(1)->create([
+            'name' => 'Trou dans le sol',
+            'id' => 1,
+        ]);
+
+        Trou::factory(1)->create([
+            'name' => 'Trou du Mans',
+            'category_id' => 1,
+            'depth' => 10,
+            'volume' => 40,
+            'location' => 'Le Mans',
+            'diameter' => 4,
+            'description' => 'Un beau trou dans l\'herbe pas loin du centre ville',
+        ]);
+
+        TrouImage::factory(1)->create([
+            'trou_id' => 1,
+            'file_path' => 'images/trou1.jpg',
+            'name' => 'Photo du Trou du Mans',
+        ]);
     }
+
 }
