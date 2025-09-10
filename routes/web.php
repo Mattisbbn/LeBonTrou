64c32d162a7fcf8e3fa2f1794dfbd8c16f4d9c2c
Mattisbbn <mattisbbn@gmail.com>
2025-09-10 17:07:06 +0200

    update

--- routes/web.php
+++ routes/web.php
@@ -5,30 +5,33 @@ use App\Models\Wind;
 
 Route::get('/', function () {
     return view('pages.home.index');
-});
+})->name('home');
 
 
 Route::get('/hlc', function () {
     return view('pages.hlc.index');
-});
+})->name('hlc');
 
 
 Route::get('/api/hlc', function () {
 
     // Fonction pour calculer le prix basé sur la vitesse du vent
-    $getPrice = function ($speed) {
+    $getPrice = function ($speed, $timestamp) {
         $price = 923 * $speed;
         $price /= 7;
         $price /= 150;
-        return $price;
+        return [
+            'price' => round($price, 2),
+            'timestamp' => $timestamp
+        ];
     };
 
     $data = Wind::all();
     $currentSpeed = $data->last()->speed;
-    $currentPrice = $getPrice($currentSpeed);
+    $currentPrice = $getPrice($currentSpeed,$data->last()->timestamp);
 
     $priceHistory = $data->map(function (Wind $item) use ($getPrice) {
-        return $getPrice($item->speed);
+        return $getPrice($item->speed,$item->timestamp);
     });
 
 
