a9b565301f70fa12a561ecf35122e88648a7212f
BertheAdrien <berthe.adrien@yahoo.fr>
2025-09-11 11:54:09 +0200

    ajout des prix calculés

--- app/Http/Controllers/TrouController.php
+++ app/Http/Controllers/TrouController.php
@@ -19,14 +19,26 @@ class TrouController extends Controller
     
         $trous = $query->get();
     
+        $price = \App\Models\Currency::latest()->first()->price ?? 0;
+    
+        // Calculer le prix pour chaque trou
+        foreach ($trous as $trou) {
+            $trou->calculatedPrice = round($trou->volume * $price, 2);
+        }
+    
         return view('pages.products.index', compact('trous'));
-    }   
+    }
+    
     
     public function show($id)
     {
         $trou = Trou::with('images', 'category', 'shipments')->findOrFail($id);
 
-        return view('pages.products.show', compact('trou'));
+        $price = \App\Models\Currency::latest()->first()->price;
+        $calculatedPrice = $trou->volume * $price;
+        $calculatedPrice = round($calculatedPrice, 2);
+
+        return view('pages.products.show', compact('trou', 'calculatedPrice'));
     }
 
 }
